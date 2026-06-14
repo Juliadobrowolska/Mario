f8f466412d4b996afa34112fceb38c59604d63b2
Juliadobrowolska <jad.books297@gmail.com>
2026-06-14 11:44:17 +0200

    map manager changes

--- Headers/MapManager.hpp
+++ Headers/MapManager.hpp
@@ -1,61 +1,46 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+
 #include <vector>
-#include <array>
-#include <random>
+#include <SFML/Graphics.hpp>
 #include "Global.hpp"
 
 class MapManager
 {
-private:
-    std::vector<Object> brick_particles;
-    std::vector<Object> mail_block_ects;
-
-    sf::Image map_sketch;
-    sf::Sprite cell_sprite;
-
-    // Tekstury ładowane z pojedynczych plików użytkownika
-    sf::Texture texture_floor1;
-    sf::Texture texture_brick;
-    sf::Texture texture_platform;
-    sf::Texture texture_activated_mail;
-    
-    // Tablice tekstur dla elementów animowanych
-    std::array<sf::Texture, 5> textures_mail_block;
-    std::array<sf::Texture, 7> textures_ects;
-
-    unsigned int animation_frame_mail;
-    unsigned int animation_frame_ects;
-    sf::Clock animation_clock;
-
-    Map map;
-
-    // Zarządzanie poziomami i losowością
-    unsigned char current_level;
-    std::vector<unsigned char> available_levels;
-    std::mt19937 random_engine;
-
 public:
+    // Struktura przechowująca dane o wrogach wykrytych na mapie pikseli
+    struct EnemyPosition
+    {
+        unsigned char type; // 0 = Ocena 2.0, 1 = Profesor
+        float x;
+        float y;
+    };
+
     MapManager();
 
-    unsigned short get_map_sketch_height() const;
-    unsigned short get_map_sketch_width() const;
+    // Główna funkcja ładująca grafikę poziomu
+    void load_level(const unsigned char i_level);
+    
+    // Funkcja sprawdzająca kolizje obiektów z otoczeniem
+    std::vector<unsigned char> map_collision(const std::vector<Cell>& i_check_cells, const sf::FloatRect& i_hit_box) const;
+
+    // Pobieranie pozycji wrogów do zespawnowania w main.cpp
+    const std::vector<EnemyPosition>& get_enemy_positions() const;
+    
+    // Czyszczenie danych przed załadowaniem nowej mapy
+    void clear_enemies();
+
+    // Gettery i Settery dla komórek mapy
+    Cell get_cell(const unsigned short i_x, const unsigned short i_y) const;
+    void set_cell(const unsigned short i_x, const unsigned short i_y, const Cell i_cell);
+    
     unsigned short get_map_width() const;
 
-    void init_level_system();
-    void load_next_random_level();
-    void load_current_level_sketch();
-
-    void add_brick_particles(const unsigned short i_x, const unsigned short i_y);
-    void add_mail_block_ects(const unsigned short i_x, const unsigned short i_y);
-    void draw_map(const bool i_draw_background, const unsigned int i_view_x, sf::RenderWindow& i_window);
-    void set_map_cell(const unsigned short i_x, const unsigned short i_y, const Cell& i_cell);
-    void set_map_size(const unsigned short i_new_size);
-    void update();
-
-    std::vector<unsigned char> map_collision(const std::vector<Cell>& i_check_cells, const sf::FloatRect& i_hitbox) const;
-    std::vector<unsigned char> map_collision(const std::vector<Cell>& i_check_cells, std::vector<sf::Vector2i>& i_collision_cells, const sf::FloatRect& i_hitbox) const;
-
-    sf::Color get_map_sketch_pixel(const unsigned short i_x, const unsigned short i_y) const;
-    Cell get_cell_at(unsigned int i_x, unsigned int i_y) const;
+private:
+    unsigned short map_width;
+    
+    // Dwuwymiarowy wektor reprezentujący siatkę klocków w grze
+    std::vector<std::vector<Cell>> map_vector;
+    
+    // Lista pozycji wrogów wyciągnięta z rysunku poziomu
+    std::vector<EnemyPosition> enemy_positions;
 };
--- MapManager.cpp
+++ MapManager.cpp
@@ -1,344 +1,162 @@
 #include <array>
-#include <chrono>
 #include <cmath>
 #include <algorithm>
-#include <random>
-#include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
+
 #include "Headers/Global.hpp"
 #include "Headers/MapManager.hpp"
 
 MapManager::MapManager() :
-    animation_frame_mail(0),
-    animation_frame_ects(0),
-    current_level(1)
+    map_width(0)
 {
-    // Ładowanie tekstur statycznych zgodnie z nazewnictwem użytkownika
-    texture_floor1.loadFromFile("Resources/Images/block1.png");
-    texture_brick.loadFromFile("Resources/Images/block2.png");
-    texture_platform.loadFromFile("Resources/Images/bookblock.png");
-    texture_activated_mail.loadFromFile("Resources/Images/mailblock4.png");
-
-    // Ładowanie animacji skrzynki pocztowej (mailblock0 do mailblock3)
-    for (int i = 0; i < 4; i++)
-    {
-        textures_mail_block[i].loadFromFile("Resources/Images/mailblock" + std::to_string(i) + ".png");
-    }
-    // Ostatnia klatka używana jako alternatywa
-    textures_mail_block[4] = texture_activated_mail;
-
-    // Ładowanie animacji punktów ECTS (ECTS1 do ECTS7)
-    for (int i = 0; i < 7; i++)
-    {
-        textures_ects[i].loadFromFile("Resources/Images/ECTS" + std::to_string(i + 1) + ".png");
-    }
-
-    // Inicjalizacja generatora liczb losowych
-    unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-    random_engine.seed(seed);
-    
-    init_level_system();
 }
 
-void MapManager::init_level_system()
+void MapManager::clear_enemies()
 {
-    current_level = 1;
-    
-    available_levels.clear();
-    available_levels.push_back(2);
-    available_levels.push_back(3);
-    
-    load_current_level_sketch();
+    enemy_positions.clear();
 }
 
-void MapManager::load_next_random_level()
+const std::vector<MapManager::EnemyPosition>& MapManager::get_enemy_positions() const
 {
-    if (available_levels.empty())
-    {
-        available_levels.push_back(1);
-        available_levels.push_back(2);
-        available_levels.push_back(3);
-    }
-
-    std::uniform_int_distribution<size_t> distribution(0, available_levels.size() - 1);
-    size_t random_index = distribution(random_engine);
-    
-    current_level = available_levels[random_index];
-    available_levels.erase(available_levels.begin() + random_index);
-    
-    load_current_level_sketch();
-}
-
-void MapManager::load_current_level_sketch()
-{
-    // Dostosowanie nazw plików do poziomu użytkownika (level_1.png, level_2.png, level_3.png)
-    map_sketch.loadFromFile("Resources/Images/level_" + std::to_string(static_cast<unsigned short>(current_level)) + ".png");
-    
-    set_map_size(map_sketch.getSize().x);
-    const unsigned int max_height = SCREEN_HEIGHT / CELL_SIZE;
-
-    for (unsigned int x = 0; x < map_sketch.getSize().x; x++)
-    {
-        for (unsigned int y = 0; y < map_sketch.getSize().y && y < max_height; y++)
-        {
-            sf::Color pixel_color = map_sketch.getPixel({x, y});
-            Cell cell_type = Cell::Empty;
-
-            if (pixel_color == sf::Color(136, 0, 21))          cell_type = Cell::Floor1; 
-            else if (pixel_color == sf::Color(237, 28, 36))     cell_type = Cell::Brick; 
-            else if (pixel_color == sf::Color(163, 73, 164))   cell_type = Cell::MailBlock; 
-            else if (pixel_color == sf::Color(255, 242, 0))     cell_type = Cell::Platform; 
-            else if (pixel_color == sf::Color(0, 162, 232))     cell_type = Cell::Ects;
-
-            map[x][y] = cell_type;
-        }
-    }
-}
-
-unsigned short MapManager::get_map_sketch_height() const
-{
-    return map_sketch.getSize().y;
-}
-
-unsigned short MapManager::get_map_sketch_width() const
-{
-    return map_sketch.getSize().x;
+    return enemy_positions;
 }
 
 unsigned short MapManager::get_map_width() const
 {
-    return map.size() * CELL_SIZE;
+    return map_width;
 }
 
-void MapManager::add_brick_particles(const unsigned short i_x, const unsigned short i_y)
+Cell MapManager::get_cell(const unsigned short i_x, const unsigned short i_y) const
 {
-    brick_particles.push_back(Object(i_x, i_y, -0.25f * BRICK_PARTICLE_SPEED, -1.5f * BRICK_PARTICLE_SPEED));
-    brick_particles.push_back(Object(i_x + 0.5f * CELL_SIZE, i_y, 0.25f * BRICK_PARTICLE_SPEED, -1.5f * BRICK_PARTICLE_SPEED));
-    brick_particles.push_back(Object(i_x, i_y + 0.5f * CELL_SIZE, -0.5f * BRICK_PARTICLE_SPEED, -BRICK_PARTICLE_SPEED));
-    brick_particles.push_back(Object(i_x + 0.5f * CELL_SIZE, i_y + 0.5f * CELL_SIZE, 0.5f * BRICK_PARTICLE_SPEED, -BRICK_PARTICLE_SPEED));
-}
-
-void MapManager::add_mail_block_ects(const unsigned short i_x, const unsigned short i_y)
-{
-    mail_block_ects.push_back(Object(i_x, i_y, 0.0f, ECTS_JUMP_SPEED));
-}
-
-void MapManager::draw_map(const bool i_draw_background, const unsigned int i_view_x, sf::RenderWindow& i_window)
-{
-    unsigned short map_start = std::floor(i_view_x / static_cast<float>(CELL_SIZE));
-    unsigned short map_end = std::ceil((SCREEN_WIDTH + i_view_x) / static_cast<float>(CELL_SIZE));
-    unsigned short map_height = SCREEN_HEIGHT / CELL_SIZE;
-
-    if (map_end > map.size()) 
+    if (i_x < map_vector.size() && i_y < map_vector[i_x].size())
     {
-        map_end = map.size();
-    }
-
-    // Rysowanie wyskakujących punktów ECTS ze skrzynek
-    if (!i_draw_background)
-    {
-        for (const Object& ects : mail_block_ects)
-        {
-            cell_sprite.setPosition({ects.x - i_view_x, ects.y});
-            cell_sprite.setTexture(textures_ects[animation_frame_ects], true);
-            // Skalowanie do rozmiaru komórki w razie potrzeby
-            sf::Vector2u texture_size = textures_ects[animation_frame_ects].getSize();
-            cell_sprite.setScale({static_cast<float>(CELL_SIZE) / texture_size.x, static_cast<float>(CELL_SIZE) / texture_size.y});
-            i_window.draw(cell_sprite);
-        }
-    }
-
-    // Przywrócenie domyślnego skalowania dla rysowania mapy
-    cell_sprite.setScale({1.0f, 1.0f});
-
-    for (unsigned short a = map_start; a < map_end; a++)
-    {
-        for (unsigned short b = 0; b < map_height; b++)
-        {
-            if (Cell::Empty == map[a][b]) continue;
-
-            cell_sprite.setPosition({static_cast<float>(CELL_SIZE * a - i_view_x), static_cast<float>(CELL_SIZE * b)});
-            sf::Vector2u t_size;
-
-            switch (map[a][b])
-            {
-                case Cell::Floor1:
-                    cell_sprite.setTexture(texture_floor1, true);
-                    break;
-                case Cell::Brick:
-                    cell_sprite.setTexture(texture_brick, true);
-                    break;
-                case Cell::Platform:
-                    cell_sprite.setTexture(texture_platform, true);
-                    break;
-                case Cell::MailBlock:
-                    cell_sprite.setTexture(textures_mail_block[animation_frame_mail], true);
-                    break;
-                case Cell::ActivatedMailBlock:
-                    cell_sprite.setTexture(texture_activated_mail, true);
-                    break;
-                case Cell::Ects:
-                    cell_sprite.setTexture(textures_ects[animation_frame_ects], true);
-                    break;
-                default:
-                    continue;
-            }
-
-            // Automatyczne dopasowanie skali tekstury do rozmiaru komórki CELL_SIZE (80x80)
-            t_size = cell_sprite.getTexture()->getSize();
-            cell_sprite.setScale({static_cast<float>(CELL_SIZE) / t_size.x, static_cast<float>(CELL_SIZE) / t_size.y});
-            
-            i_window.draw(cell_sprite);
-        }
-    }
-
-    // Rysowanie cząstek rozbitych bloków
-    if (!i_draw_background)
-    {
-        cell_sprite.setTexture(texture_brick, true);
-        sf::Vector2u b_size = texture_brick.getSize();
-        
-        for (const Object& brick_particle : brick_particles)
-        {
-            cell_sprite.setPosition({brick_particle.x - i_view_x, brick_particle.y});
-            // Cząstki rysowane są jako pomniejszone wersje tekstury bazowej
-            cell_sprite.setScale({(static_cast<float>(CELL_SIZE) / b_size.x) * 0.5f, (static_cast<float>(CELL_SIZE) / b_size.y) * 0.5f});
-            i_window.draw(cell_sprite);
-        }
-    }
-    
-    cell_sprite.setScale({1.0f, 1.0f});
-}
-
-void MapManager::set_map_cell(const unsigned short i_x, const unsigned short i_y, const Cell& i_cell)
-{
-    if (i_x < map.size() && i_y < map[0].size())
-    {
-        map[i_x][i_y] = i_cell;
-    }
-}
-
-void MapManager::set_map_size(const unsigned short i_new_size)
-{
-    map.clear();
-    map.resize(i_new_size);
-    for (auto& column : map)
-    {
-        column.fill(Cell::Empty);
-    }
-}
-
-void MapManager::update()
-{
-    // Aktualizacja klatek animacji na podstawie zegara
-    if (animation_clock.getElapsedTime().asMilliseconds() > 150)
-    {
-        animation_frame_mail = (animation_frame_mail + 1) % 4;
-        animation_frame_ects = (animation_frame_ects + 1) % 7;
-        animation_clock.restart();
-    }
-
-    for (Object& ects : mail_block_ects)
-    {
-        ects.vertical_speed += GRAVITY;
-        ects.y += ects.vertical_speed;
-    }
-
-    for (Object& brick_particle : brick_particles)
-    {
-        brick_particle.vertical_speed += GRAVITY;
-        brick_particle.x += brick_particle.horizontal_speed;
-        brick_particle.y += brick_particle.vertical_speed;
-    }
-
-    brick_particles.erase(std::remove_if(brick_particles.begin(), brick_particles.end(), [](const Object& i_brick_particle)
-    {
-        return SCREEN_HEIGHT <= i_brick_particle.y;
-    }), brick_particles.end());
-
-    mail_block_ects.erase(std::remove_if(mail_block_ects.begin(), mail_block_ects.end(), [](const Object& i_ects)
-    {
-        return 0.0f <= i_ects.vertical_speed;
-    }), mail_block_ects.end());
-}
-
-std::vector<unsigned char> MapManager::map_collision(const std::vector<Cell>& i_check_cells, const sf::FloatRect& i_hitbox) const
-{
-    std::vector<unsigned char> output;
-    const short top_tile = std::floor(i_hitbox.top / CELL_SIZE);
-    const short bottom_tile = std::floor((std::ceil(i_hitbox.height + i_hitbox.top) - 1) / CELL_SIZE);
-    const short left_tile = std::floor(i_hitbox.left / CELL_SIZE);
-    const short right_tile = std::floor((std::ceil(i_hitbox.left + i_hitbox.width) - 1) / CELL_SIZE);
-
-    for (short a = top_tile; a <= bottom_tile; a++)
-    {
-        output.push_back(0);
-        for (short b = left_tile; b <= right_tile; b++)
-        {
-            if (0 <= b && b < static_cast<short>(map.size()))
-            {
-                if (0 <= a && a < static_cast<short>(map[0].size()))
-                {
-                    if (i_check_cells.end() != std::find(i_check_cells.begin(), i_check_cells.end(), map[b][a]))
-                    {
-                        output[a - top_tile] += std::pow(2, right_tile - b);
-                    }
-                }
-            }
-            else if (i_check_cells.end() != std::find(i_check_cells.begin(), i_check_cells.end(), Cell::Wall))
-            {
-                output[a - top_tile] += std::pow(2, right_tile - b);
-            }
-        }
-    }
-    return output;
-}
-
-std::vector<unsigned char> MapManager::map_collision(const std::vector<Cell>& i_check_cells, std::vector<sf::Vector2i>& i_collision_cells, const sf::FloatRect& i_hitbox) const
-{
-    std::vector<unsigned char> output;
-    i_collision_cells.clear();
-
-    const short top_tile = std::floor(i_hitbox.top / CELL_SIZE);
-    const short bottom_tile = std::floor((std::ceil(i_hitbox.height + i_hitbox.top) - 1) / CELL_SIZE);
-    const short left_tile = std::floor(i_hitbox.left / CELL_SIZE);
-    const short right_tile = std::floor((std::ceil(i_hitbox.left + i_hitbox.width) - 1) / CELL_SIZE);
-
-    for (short a = top_tile; a <= bottom_tile; a++)
-    {
-        output.push_back(0);
-        for (short b = left_tile; b <= right_tile; b++)
-        {
-            if (0 <= b && b < static_cast<short>(map.size()))
-            {
-                if (0 <= a && a < static_cast<short>(map[0].size()))
-                {
-                    if (i_check_cells.end() != std::find(i_check_cells.begin(), i_check_cells.end(), map[b][a]))
-                    {
-                        i_collision_cells.push_back(sf::Vector2i(b, a));
-                        output[a - top_tile] += std::pow(2, right_tile - b);
-                    }
-                }
-            }
-            else if (i_check_cells.end() != std::find(i_check_cells.begin(), i_check_cells.end(), Cell::Wall))
-            {
-                output[a - top_tile] += std::pow(2, right_tile - b);
-            }
-        }
-    }
-    return output;
-}
-
-sf::Color MapManager::get_map_sketch_pixel(const unsigned short i_x, const unsigned short i_y) const
-{
-    return map_sketch.getPixel({i_x, i_y});
-}
-
-Cell MapManager::get_cell_at(unsigned int i_x, unsigned int i_y) const
-{
-    if (i_x < map.size() && i_y < (SCREEN_HEIGHT / CELL_SIZE))
-    {
-        return map[i_x][i_y];
+        return map_vector[i_x][i_y];
     }
     return Cell::Empty;
+}
+
+void MapManager::set_cell(const unsigned short i_x, const unsigned short i_y, const Cell i_cell)
+{
+    if (i_x < map_vector.size() && i_y < map_vector[i_x].size())
+    {
+        map_vector[i_x][i_y] = i_cell;
+    }
+}
+
+// Ładowanie poziomu na podstawie numeru (np. i_level = 1 odpali level_1.png)
+void MapManager::load_level(const unsigned char i_level)
+{
+    sf::Image map_image;
+    
+    // Ścieżka skrócona bezpośrednio do folderu Resources/
+    if (!map_image.loadFromFile("Resources/level_" + std::to_string(i_level) + ".png"))
+    {
+        return; 
+    }
+
+    map_width = map_image.getSize().x;
+    
+    // Czyszczenie poprzednich danych mapy i wrogów
+    map_vector.clear();
+    enemy_positions.clear();
+    map_vector.resize(map_width, std::vector<Cell>(SCREEN_HEIGHT / CELL_SIZE, Cell::Empty));
+
+    // Iteracja po każdym pikselu obrazu poziomu
+    for (unsigned short x = 0; x < map_image.getSize().x; x++)
+    {
+        for (unsigned short y = 0; y < map_image.getSize().y; y++)
+        {
+            sf::Color pixel_color = map_image.getPixel(x, y);
+
+            // 1. CZARNY (0, 0, 0) -> Block1 (Stabilna podłoga)
+            if (pixel_color == sf::Color(0, 0, 0))
+            {
+                map_vector[x][y] = Cell::Wall;
+            }
+            // 2. CZERWONY (237, 28, 36) -> Block1 wiszący (Cegła do niszczenia głową)
+            else if (pixel_color == sf::Color(237, 28, 36))
+            {
+                map_vector[x][y] = Cell::Brick;
+            }
+            // 3. BORDOWY (136, 0, 21) -> Block2 (Dolna podłoga bez autouzupełniania dziur)
+            else if (pixel_color == sf::Color(136, 0, 21))
+            {
+                map_vector[x][y] = Cell::Wall;
+            }
+            // 4. ŻÓŁTY (255, 242, 0) -> BookBlock (Platforma z książkami)
+            else if (pixel_color == sf::Color(255, 242, 0))
+            {
+                map_vector[x][y] = Cell::Platform;
+            }
+            // 5. FIOLETOWY (163, 73, 164) -> MailBlock (Skrzynka pocztowa z lootem)
+            else if (pixel_color == sf::Color(163, 73, 164))
+            {
+                map_vector[x][y] = Cell::MailBlock;
+            }
+            // 6. JASNONIEBIESKI (0, 162, 232) -> Punkt spawnu wrogów (50% Ocena 2.0 / 50% Profesor)
+            else if (pixel_color == sf::Color(0, 162, 232))
+            {
+                map_vector[x][y] = Cell::Empty; // Sam kafel musi być pusty, żeby wróg mógł stać
+                
+                // Losowanie typu wroga: 0 = Ocena 2.0, 1 = Profesor
+                unsigned char enemy_type = (rand() % 2 == 0) ? 0 : 1;
+                enemy_positions.push_back({enemy_type, static_cast<float>(CELL_SIZE * x), static_cast<float>(CELL_SIZE * y)});
+            }
+        }
+    }
+}
+
+// System wykrywania kolizji obiektów z kafelkami na mapie
+std::vector<unsigned char> MapManager::map_collision(const std::vector<Cell>& i_check_cells, const sf::FloatRect& i_hit_box) const
+{
+    std::vector<unsigned char> output(4, 0);
+
+    // Przeliczanie pozycji z pikseli na indeksy kafelków w siatce
+    short start_x = std::max(0.0f, floor(i_hit_box.left / CELL_SIZE));
+    short end_x = std::min(static_cast<float>(map_width - 1), ceil((i_hit_box.left + i_hit_box.width) / CELL_SIZE));
+    short start_y = std::max(0.0f, floor(i_hit_box.top / CELL_SIZE));
+    short end_y = std::min(static_cast<float>(SCREEN_HEIGHT / CELL_SIZE - 1), ceil((i_hit_box.top + i_hit_box.height) / CELL_SIZE));
+
+    for (short x = start_x; x < end_x; x++)
+    {
+        for (short y = start_y; y < end_y; y++)
+        {
+            // Sprawdzamy, czy dany kafelek blokuje ruch
+            if (std::end(i_check_cells) != std::find(i_check_cells.begin(), i_check_cells.end(), map_vector[x][y]))
+            {
+                // Kolizja z lewej strony kafelka
+                if (i_hit_box.left + i_hit_box.width - horizontal_speed > CELL_SIZE * x && i_hit_box.left - horizontal_speed < CELL_SIZE * (1 + x))
+                {
+                    // Kolizja od góry kafelka (stanięcie na ziemi)
+                    if (i_hit_box.top + i_hit_box.height - vertical_speed <= CELL_SIZE * y)
+                    {
+                        output[0] = 1; 
+                    }
+                    // Kolizja od dołu kafelka (uderzenie głową)
+                    else if (i_hit_box.top - vertical_speed >= CELL_SIZE * (1 + y))
+                    {
+                        output[1] = 1; 
+                    }
+                }
+
+                // Kolizja z górną/dolną krawędzią kafelka
+                if (i_hit_box.top + i_hit_box.height - vertical_speed > CELL_SIZE * y && i_hit_box.top - vertical_speed < CELL_SIZE * (1 + y))
+                {
+                    // Kolizja z prawej strony kafelka
+                    if (i_hit_box.left + i_hit_box.width - horizontal_speed <= CELL_SIZE * x)
+                    {
+                        output[2] = 1; 
+                    }
+                    // Kolizja z lewej strony kafelka
+                    else if (i_hit_box.left - horizontal_speed >= CELL_SIZE * (1 + x))
+                    {
+                        output[3] = 1; 
+                    }
+                }
+            }
+        }
+    }
+
+    return output;
 }
